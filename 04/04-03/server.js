8fe39b5b6404ae859997eb0e9ded8bae56657f2d
YestoryFornever <guosj90@163.com>
2017-06-28 16:50:43 +0800

    处理404
    fs.stat

--- 04/04-03/server.js
+++ 04/04-03/server.js
@@ -8,20 +8,25 @@ let root = __dirname;
 let server = http.createServer(( req, res )=>{
 	let url = parse( req.url );
 	let path = join( root, url.pathname );
-	let stream = fs.createReadStream(path);
-	/**普通方法
-	stream.on('data',(chunk)=>{
-		res.write(chunk);
-	});
-	stream.on('end',()=>{
-		res.end();
-	});
-	*/
-	/**pipe 优化后的方法*/
-	stream.pipe(res);
-	stream.on('error',(err)=>{
-		res.statusCode = 500;
-		res.end('Internal Server Error');//服务器错误
+	
+	fs.stat(path,(err,stat)=>{
+		if(err){
+			if('ENOENT'==err.code){
+				res.statusCode = 404;
+				res.end('Not Found');
+			}else{
+				res.statusCode = 500;
+				res.end('Internal Server Error');
+			}
+		}else{
+			res.setHeader('Content-Length',stat.size);
+			let stream = fs.createReadStream(path);
+			stream.pipe(res);
+			stream.on('error',(err)=>{
+				res.statusCode = 500;
+				res.end('Internal Server Error');
+			});
+		}
 	});
 });
 server.listen(9999);
