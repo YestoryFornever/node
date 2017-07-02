e8eec20d858f13451ac630dd40a747eec57d6190
YestoryFornever <guosj90@163.com>
2017-07-02 11:43:44 +0800

    authorization

--- 06/0601/server.js
+++ 06/0601/server.js
@@ -1,5 +1,9 @@
 let connect = require('connect');
-let app = connect().use(logger).use(hello).listen(9999);
+connect()
+.use(logger)
+.use(restrict)
+.use(hello)
+.listen(9999);
 console.log(9999);
 
 function logger(req,res,next) {
@@ -10,4 +14,19 @@ function logger(req,res,next) {
 function hello(req,res) {
 	res.setHeader('Content-Type','text/plain');
 	res.end('hello world');
+}
+
+function restrict(req,res,next) {
+	var authorization = req.headers.authorization;
+	if(!authorization) return next(new Error('Unauthorized!'));
+	var parts = authorization.split(' ');
+	var scheme = parts[0];
+	var auth = new Buffer(parts[1],'base64').toString().split(':');
+	var user = auth[0];
+	var pass = auth[1];
+
+	authenticatedWithDataBase(user,pass,function(err){
+		if(err) return next(err);
+		next();
+	});
 }
