7ab3fb1a8ee84f519e4c5e80e11eea4a28ec1d9a
YestoryFornever <guosj90@163.com>
2017-07-03 10:41:17 +0800

    关闭  读取

--- 00/00-04/fs.js
+++ 00/00-04/fs.js
@@ -7,4 +7,12 @@ fs.open('../input.txt','r+',(err,fd)=>{
 		return console.error(err);
 	}
 	console.log('文件打开成功');
+	//关闭文件
+	console.log('准备关闭文件');
+	fs.close(fd,(err)=>{
+		if(err){
+			console.log(err);
+		}
+		console.log('文件关闭成功');
+	});
 });
