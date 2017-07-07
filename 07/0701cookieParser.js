2ad339336e0063108e008ab707e5a4092dd78ea1
YestoryFornever <guosj90@163.com>
2017-07-07 11:08:45 +0800

    cookieParser rename

--- 07/0701cookieParser.js
+++ 07/0701cookieParser.js
@@ -4,9 +4,12 @@ let cookieParser = require('cookie-parser');
 connect()
 .use(cookieParser('tobi is a cool ferret'))
 .use((req,res)=>{
-	console.log(req.cookies);
+	/*console.log(req.cookies);
 	console.log(req.signedCookies);
-	res.end('Hello\n');
+	res.end('Hello\n');*/
+	res.setHeader('Set-Cookie','foo=bar');
+	res.setHeader('Set-Cookie','tobi=ferret;Expires=Tue, 08 Jun 2021 10:18:14 GMT');
+	res.end();
 })
 .listen(9999);
 console.log(9999);
