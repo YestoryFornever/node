a2e4da13485207a702683dc7042f071d7bd82da9
YestoryFornever <guosj90@163.com>
2017-07-29 17:55:53 +0800

    提交表单
    先去看看热部署的东西，改的太烦了
    
    tips chokidar

--- 09/shoutbox/routes/users.js
+++ 09/shoutbox/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../lib/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,6 +10,30 @@ router.get('/', function(req, res, next) {
 router.get('/register', function(req, res, next) {
 	// res.send('注册');
 	res.render('register',{title:'注册'});
+});
+
+router.post('/register', function(req, res, next) {
+	console.log(req.body);
+	var data = req.body.user;
+	console.log(data);
+	User.getByName(data.name, function(err, user){
+		if(err) return next(err);
+		if(user.id){
+			res.error('用户名已被注册');
+			res.redirect('back');
+		}else{
+			user = new User({
+				name: data.name,
+				pass: data.pass
+			});
+
+			user.save(function(err){
+				if(err) return next(err);
+				req.session.uid = user.id;
+				res.redirect('/');
+			});
+		}
+	})
 });
 
 module.exports = router;
