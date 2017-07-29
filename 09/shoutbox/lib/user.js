77f66cef4a7d6a642470520007508cb1af2b24ba
YestoryFornever <guosj90@163.com>
2017-07-29 13:18:41 +0800

    bug fix
    prototype属性上的方法不能使用箭头函数

--- 09/shoutbox/lib/user.js
+++ 09/shoutbox/lib/user.js
@@ -1,5 +1,5 @@
-let redis = require('redis');
-let bcrypt = require('bcrypt');
+let redis = require('redis');//NoSQL数据库
+let bcrypt = require('bcrypt');//哈希
 let db = redis.createClient();
 
 module.exports = User;
@@ -10,52 +10,51 @@ function User(obj){
 	}
 }
 
-User.prototype.save = (fn)=>{
-	if(this.id){
+User.prototype.save = function(fn){
+	if(this.id){//用户已存在
 		this.update(fn);
 	}else{
 		var user = this;
-		db.incr('user:ids',(err,id)=>{
+		db.incr('user:ids',(err,id)=>{//创建唯一id
 			if(err) return fn(err);
-			user.id = id;
+			user.id = id;//设定id，以便保存
 			user.hashPassword((err)=>{
 				if(err) return fn(err);
-				user.update(fn);
+				user.update(fn);//保存用户属性
 			});
 		});
 	}
 }
 
-User.prototype.update = (fn)=>{
+User.prototype.update = function(fn){
 	var user = this;
 	var id = user.id;
-	db.set('user:id:'+user.name,id,(err)=>{
+	db.set('user:id:'+user.name,id,(err)=>{//用名称索引用户id
 		if(err) return fn(err);
-		db.hmset('user:'+id,user,(err)=>{
+		db.hmset('user:'+id,user,(err)=>{//用Redis哈希存储数据
 			fn(err);
 		});
 	});
 }
-
-User.prototype.hashPassword = (fn)=>{
+User.prototype.hashPassword = function(fn){
 	var user = this;
 	bcrypt.genSalt(12,(err,salt)=>{//加盐，干扰彩虹表攻击
 		if(err) return fn(err);
-		user.salt = salt;
+		user.salt = salt;//设定盐以便保存
 		bcrypt.hash(user.pass, salt, (err,hash)=>{
 			if(err) return fn(err);
-			user.pass = hash;
+			user.pass = hash;//设定哈希以便保存
 			fn();
 		});
 	});
 };
 
-var tobi = new User({
-	name:'Tobi',
-	pass:'im a ferret',
-	age:'2'
+/*var tobi = new User({
+	name:'guoshijian',
+	pass:'sunwukong',
+	age:'22'
 });
 tobi.save((err)=>{
 	if(err) throw err;
 	console.log('user id %d', tobi.id);
-})
+});*/
