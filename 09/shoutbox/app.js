e933ce30787ba0ef5ca7571abfdf167eb87c4594
YestoryFornever <guosj90@163.com>
2017-07-29 17:23:13 +0800

    错误信息处理

--- 09/shoutbox/app.js
+++ 09/shoutbox/app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var messages = require('./lib/messages');
+
 var app = express();
 
 // view engine setup
@@ -20,6 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(messages);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
