29e510f1a8ae49f1e86d1885c714c5a411200351
YestoryFornever <guosj90@163.com>
2017-07-29 17:10:29 +0800

    添加注册页面及路由

--- 09/shoutbox/routes/users.js
+++ 09/shoutbox/routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	res.send('respond with a resource');
+});
+
+router.get('/register', function(req, res, next) {
+	// res.send('注册');
+	res.render('register',{title:'注册'});
 });
 
 module.exports = router;
