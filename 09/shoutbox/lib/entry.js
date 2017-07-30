5eae72fb5c1e4af3ae5abac8ed62f6a6e93f1e7a
YestoryFornever <guosj90@163.com>
2017-07-30 08:07:57 +0800

    消息 entry

---
+++ 09/shoutbox/lib/entry.js
@@ -0,0 +1,29 @@
+var redis = require('redis');
+var db = redis.createClient();
+
+module.exports = Entry;
+
+function Entry(obj){
+	for(let key in obj){
+		this[key] = obj[key];
+	}
+}
+
+Entry.prototype.save = function(fn){
+	var entryJSON = JSON.stringify(this);
+	db.lpush('entries',entryJSON,(err)=>{
+		if(err) return fn(err);
+		fn();
+	});
+}
+
+Entry.getRange = function(from,to,fn){
+	db.lrange('entries',from,to,(err,items)=>{
+		if(err) return fn(err);
+		var entries = [];
+		items.forEach((item)=>{
+			entries.push(JSON.parse(item));
+		});
+		fn(null,entries);
+	});
+}
