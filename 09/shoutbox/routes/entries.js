92743bf2ef5d8c4375726bc42cd68bba9527b067
YestoryFornever <guosj90@163.com>
2017-07-30 10:44:06 +0800

    见描述
    menu中的链接应该用绝对路径；
    post／get

--- 09/shoutbox/routes/entries.js
+++ 09/shoutbox/routes/entries.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var Entry = require('../lib/entry');
 
-router.get('/post', function(req, res, next) {
+router.get('/', function(req, res, next) {
 	Entry.getRange(0,-1,(err,entries)=>{
 		if(err) return next(err);
 		res.render('entries',{
@@ -11,6 +11,25 @@ router.get('/post', function(req, res, next) {
 			entries:entries,
 		});
 	});
+});
+
+router.get('/post', function(req, res, next) {
+	res.render('post',{title:'Post'});
+});
+
+router.post('/post',function(req, res, next) {
+	var data = req.body;
+
+	var entry = new Entry({
+		'username':res.locals.user.name,
+		'title':data.title,
+		'body':data.body
+	});
+
+	entry.save((err)=>{
+		if(err) return next(err);
+		res.redirect('./');
+	})
 });
 
 module.exports = router;
