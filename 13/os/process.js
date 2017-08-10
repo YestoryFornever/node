721dda501cc8b7960a747cf6b1d6ca7ea9175320
yestoryfornever <guosj90@163.com>
2017-08-10 14:52:28 +0800

    debug

---
+++ 13/os/process.js
@@ -0,0 +1,12 @@
+var debug;
+if(process.env.DEBUG){
+	debug = function(data){
+		console.error(data);
+	};
+}else{
+	debug = function(){};
+}
+
+debug('this is a debug call');
+console.log('HW');
+debug('this is another debug call');
